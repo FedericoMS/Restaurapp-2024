9a2bca39324a237e2123948765ac15af215de459
Fedecardozo <Fedecardozo.a@gmail.com>
2024-10-27 20:53:44 -0300

    Vuelvo el splash a su lugar

--- src/app/splash/splash.page.ts
+++ src/app/splash/splash.page.ts
@@ -24,11 +24,8 @@ import { Router } from '@angular/router';
 })
 export class SplashPage implements OnInit {
   constructor(public router: Router) {
-    // setTimeout(() => {
-    //   this.router.navigateByUrl('login', { replaceUrl: true }); //El replaceUrl es para que NO se pueda volver a la pantalla de splash cuando volvés en el celu
-    // }, 3000);
     setTimeout(() => {
-      this.router.navigateByUrl('alta-empleado', { replaceUrl: true }); //El replaceUrl es para que NO se pueda volver a la pantalla de splash cuando volvés en el celu
+      this.router.navigateByUrl('login', { replaceUrl: true }); //El replaceUrl es para que NO se pueda volver a la pantalla de splash cuando volvés en el celu
     }, 3000);
   }
 
