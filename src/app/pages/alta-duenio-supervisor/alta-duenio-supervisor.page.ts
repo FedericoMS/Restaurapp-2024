da4a30eb06a2fcdbabba6fe76151c582e56f91ce
Lautigarcia17 <lauti1718garcia@gmail.com>
2024-10-29 11:18:17 -0300

    Alta dueño-supervisor finalizada

--- src/app/pages/alta-duenio-supervisor/alta-duenio-supervisor.page.ts
+++ src/app/pages/alta-duenio-supervisor/alta-duenio-supervisor.page.ts
@@ -7,24 +7,27 @@ import { UtilService } from 'src/app/services/util.service';
 import { Empleado } from 'src/app/clases/empleado';
 import { Alert } from 'src/app/clases/alert';
 import { addIcons } from 'ionicons';
-import { cameraOutline, qrCodeOutline} from 'ionicons/icons'
+import { cameraOutline, checkmark, closeOutline, qrCodeOutline} from 'ionicons/icons'
 import { cuilValidator } from 'src/app/validators/cuilValidator';
+import { SpinnerComponent } from 'src/app/components/spinner/spinner.component';
 @Component({
   selector: 'app-alta-duenio-supervisor',
   templateUrl: './alta-duenio-supervisor.page.html',
   styleUrls: ['./alta-duenio-supervisor.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, FormsModule, ReactiveFormsModule]
+  imports: [SpinnerComponent,IonIcon, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, FormsModule, ReactiveFormsModule]
 })
 export class AltaDuenioSupervisorPage implements OnInit {
   fg!: FormGroup;
   list_roles = Empleado.get_roles();
   foto_url: string = '';
   img?: string = '';
+  isLoading: boolean;
 
 
   constructor(private firestore : FirestoreService, private utilService : UtilService, private fb : FormBuilder){
-    addIcons({qrCodeOutline,cameraOutline})
+    addIcons({qrCodeOutline,cameraOutline,closeOutline,checkmark})
+    this.isLoading = false;
     this.fg = this.fb.group({
       nombre: ['', [Validators.required]],
       apellido: ['', [Validators.required]],
@@ -65,6 +68,7 @@ export class AltaDuenioSupervisorPage implements OnInit {
 
   async cargar() {
     if (this.fg.valid) {
+      this.isLoading = true;
       await this.upload_storage();
 
       this.firestore
@@ -81,12 +85,14 @@ export class AltaDuenioSupervisorPage implements OnInit {
         .then(() => {
           Alert.success('Se cargo exitosamente el empleado', '');
           this.fg.reset();
+          this.isLoading = false;
         })
         .catch(() => {
           Alert.error(
             'Hubo un problema al cargar el empleado',
             'Vuelva a intentar más tarde'
           );
+          this.isLoading = false;
         });
     } else {
       Alert.error(
@@ -110,26 +116,23 @@ export class AltaDuenioSupervisorPage implements OnInit {
   async upload_storage() {
     if (this.img) {
       this.foto_url = await this.firestore.uploadImage(
-        `imagenes_empelados/${Date.now()}`,
+        `imagenes_dueños_supervisores/${Date.now()}`,
         this.img
       );
     }
   }
   allowOnlyNumbers(event: KeyboardEvent) {
     const charCode = event.charCode;
-    // Evita que se ingresen caracteres que no sean números
     if (charCode < 48 || charCode > 57) {
       event.preventDefault();
     }
   }
   
-  // También puedes agregar validación en la función formatCuil
   formatCuil() {
     let rawValue = this.fg.get('cuil')?.value.replace(/-/g, '');
     
-    // Validar que solo contenga números
     if (!/^\d*$/.test(rawValue)) {
-      rawValue = rawValue.replace(/\D/g, ''); // Elimina cualquier carácter no numérico
+      rawValue = rawValue.replace(/\D/g, '');
     }
   
     if (rawValue.length > 2) {
