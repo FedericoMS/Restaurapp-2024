707c18bea9cdac915d97166cafe23ed8cfb46b94
Fedecardozo <Fedecardozo.a@gmail.com>
2024-10-30 10:16:07 -0300

    page encuesta empleados

--- src/app/pages/encuestas/encuesta-empleados/encuesta-empleados.page.ts
+++ src/app/pages/encuestas/encuesta-empleados/encuesta-empleados.page.ts
@@ -1,20 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { Encuesta } from 'src/app/clases/encuesta';
+import { FirestoreService } from 'src/app/services/firestore.service';
 
 @Component({
   selector: 'app-encuesta-empleados',
   templateUrl: './encuesta-empleados.page.html',
   styleUrls: ['./encuesta-empleados.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class EncuestaEmpleadosPage implements OnInit {
-
-  constructor() { }
+  fire = inject(FirestoreService);
+  constructor() {}
 
   ngOnInit() {
-  }
+    const encuesta: Encuesta = new Encuesta({
+      question: '¿Cómo describiría el ambiente del restaurante?',
+      Tranquilo: 0,
+      Animado: 0,
+      Ruidoso: 0,
+    });
 
+    // this.fire.addEncuesta(encuesta);
+  }
 }
