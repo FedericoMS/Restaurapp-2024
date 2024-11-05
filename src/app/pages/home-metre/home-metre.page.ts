769d3240f27f0e70d0d3496342840f204e40dd9d
Lautigarcia17 <lauti1718garcia@gmail.com>
2024-11-05 02:04:13 -0300

    Avance home-metre

--- src/app/pages/home-metre/home-metre.page.ts
+++ src/app/pages/home-metre/home-metre.page.ts
@@ -1,20 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonFab, IonFabButton, IonFabList, IonIcon } from '@ionic/angular/standalone';
+import { UserService } from 'src/app/services/user.service';
+import { FirestoreService } from 'src/app/services/firestore.service';
+import { arrowBackCircleOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 
 @Component({
   selector: 'app-home-metre',
   templateUrl: './home-metre.page.html',
   styleUrls: ['./home-metre.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonIcon, IonFabList, IonFabButton, IonFab, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class HomeMetrePage implements OnInit {
-
-  constructor() { }
+  listUsersWaiting! : Array<any>;
+  listTables! : Array<any>;
+  customer : any;
+  constructor(public userService: UserService, private firestore : FirestoreService) { 
+    this.listUsersWaiting = [];
+    // this.customer = 'Lautaro Garcia';
+    this.listTables = [];
+    this.customer = null;
+  }
 
   ngOnInit() {
+    addIcons({ arrowBackCircleOutline});
+
+
+    this.firestore.getCollection('lista_de_espera').valueChanges()
+    .subscribe((response)=>{
+      this.listUsersWaiting = response;
+    })
+
+    this.firestore.getCollection('mesas').valueChanges()
+    .subscribe((response)=>{
+      this.listTables = response.sort((a : any, b : any) => a.numero - b.numero);
+    })
+
+
   }
+
+
+  setTable(user : any){
+    this.customer = user;
+  }
+
+
+  goBack(){
+    this.customer = null;
+  }
+
 
 }
