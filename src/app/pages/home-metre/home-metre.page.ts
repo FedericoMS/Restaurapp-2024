6562a43ffa9b0221dfa20b123facf0f4d0f5b221
Lautigarcia17 <lauti1718garcia@gmail.com>
2024-11-06 00:48:15 -0300

    finalice el home-metre

--- src/app/pages/home-metre/home-metre.page.ts
+++ src/app/pages/home-metre/home-metre.page.ts
@@ -6,19 +6,22 @@ import { UserService } from 'src/app/services/user.service';
 import { FirestoreService } from 'src/app/services/firestore.service';
 import { arrowBackCircleOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
+import Swal from 'sweetalert2';
+import { SpinnerComponent } from 'src/app/components/spinner/spinner.component';
 
 @Component({
   selector: 'app-home-metre',
   templateUrl: './home-metre.page.html',
   styleUrls: ['./home-metre.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonFabList, IonFabButton, IonFab, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonIcon, IonFabList, IonFabButton, IonFab, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,SpinnerComponent]
 })
 export class HomeMetrePage implements OnInit {
   listUsersWaiting : Array<any>;
   listTables : Array<any>;
   customer : any;
   idUserWaiting : string;
+  isLoading! : boolean;
 
   constructor(public userService: UserService, private firestore : FirestoreService) { 
     this.listUsersWaiting = [];
@@ -31,10 +34,11 @@ export class HomeMetrePage implements OnInit {
   ngOnInit() {
     addIcons({ arrowBackCircleOutline});
 
-
+    this.isLoading = true;
     this.firestore.getCollection('lista_de_espera').valueChanges()
     .subscribe((response)=>{
       this.listUsersWaiting = response;
+      this.isLoading = false;
     })
 
     this.firestore.getCollection('mesas').valueChanges()
@@ -53,20 +57,31 @@ export class HomeMetrePage implements OnInit {
 
   saveTable(table : any){
     if(this.customer.cliente !== '' && this.customer.id_cliente !== '' && this.idUserWaiting !== ''){
-      table.nombreCliente = this.customer.cliente;
-      table.idCliente = this.customer.id_cliente;
-      table.estado = 'ocupada';
-      
-      this.firestore.updateDatabase('mesas',table);
-      this.firestore.removeObjectDatabase('lista_de_espera',this.idUserWaiting);
-      this.emptyValues();
-      this.userService.showToast(
-        'Se asigno la mesa' + table.numero + ' al usuario ' + table.nombreCliente,
-        'lightgreen',
-        'center',
-        'success',
-        'black'
-      );
+
+      Swal.fire({
+        title: "¿Estás seguro de que quieres aprobar a este cliente?",
+        showCancelButton: true,
+        confirmButtonText: "Aprobar",
+        cancelButtonText: `Cancelar`,
+        heightAuto: false
+      }).then((result) => {
+        if (result.isConfirmed) {
+          table.nombreCliente = this.customer.cliente;
+          table.idCliente = this.customer.id_cliente;
+          table.estado = 'ocupada';
+          
+          this.firestore.updateDatabase('mesas',table);
+          this.firestore.removeObjectDatabase('lista_de_espera',this.idUserWaiting);
+          this.emptyValues();
+          this.userService.showToast(
+            'Se asigno la mesa' + table.numero + ' al usuario ' + table.nombreCliente,
+            'lightgreen',
+            'center',
+            'success',
+            'black'
+          );
+        }
+      });
     }
   }
 
