f00a0e36a59867dc204e3422ff8668a3fc768b3b
FedericoMS <fededeath15@gmail.com>
2024-11-09 09:59:24 -0300

    actualizo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,7 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       SplashScreen.hide(); // Hide the splash screen
-      this.router.navigateByUrl('splash'); // Navigate to the splash route
+      this.router.navigateByUrl('login'); // Navigate to the splash route
     });
   }
 }
