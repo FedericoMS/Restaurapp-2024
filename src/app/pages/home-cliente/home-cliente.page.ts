11524c0a37bb449b2d8f6f2e0c5a6e45e735d2be
Fedecardozo <Fedecardozo.a@gmail.com>
2024-11-09 20:29:42 -0300

    agrego para eliminar la subscripcion del home-cliente

--- src/app/pages/home-cliente/home-cliente.page.ts
+++ src/app/pages/home-cliente/home-cliente.page.ts
@@ -55,7 +55,7 @@ export class HomeClientePage implements OnInit {
   fire = inject(FirestoreService);
   userService = inject(UserService);
   mesaAsignada = this.userService.nroMesa > 0;
-  sub_mesas?: Subscription;
+  sub_user?: Subscription;
   sub_pedidos?: Subscription;
   pedido?: Pedido;
 
@@ -63,36 +63,38 @@ export class HomeClientePage implements OnInit {
     addIcons({ qrCodeOutline, chatbubbleOutline });
   }
   ngOnDestroy(): void {
-    this.sub_mesas?.unsubscribe();
+    this.sub_user?.unsubscribe();
     this.sub_pedidos?.unsubscribe();
   }
 
   ngOnInit() {
-    this.fire.getUserProfile2(this.userService.uidUser).subscribe((next) => {
-      const aux: Usuario = next as Usuario;
-      this.cliente = aux;
-      this.userService.nroMesa = aux.nroMesa;
+    this.sub_user = this.fire
+      .getUserProfile2(this.userService.uidUser)
+      .subscribe((next) => {
+        const aux: Usuario = next as Usuario;
+        this.cliente = aux;
+        this.userService.nroMesa = aux.nroMesa;
 
-      if (aux.lista_espera) {
-        this.flagMesa = true;
-      }
-      if (aux.nroMesa) {
-        this.msj = 'Su mesa es la número ' + aux.nroMesa;
-        this.msjColor = 'primary';
-        if(this.userService.nroMesa !== aux.nroMesa){
-          console.log("NG ONINIT");
-          this.fire.updateUser(this.cliente);
+        if (aux.lista_espera) {
+          this.flagMesa = true;
         }
-      } else {
-        this.msj = 'Mesa sin asignar';
-        this.msjColor = 'danger';
-      }
-    });
+        if (aux.nroMesa) {
+          this.msj = 'Su mesa es la número ' + aux.nroMesa;
+          this.msjColor = 'primary';
+          if (this.userService.nroMesa !== aux.nroMesa) {
+            console.log('NG ONINIT');
+            this.fire.updateUser(this.cliente);
+          }
+        } else {
+          this.msj = 'Mesa sin asignar';
+          this.msjColor = 'danger';
+        }
+      });
     this.get_pedidos();
   }
 
   get_pedidos() {
-    this.sub_mesas = this.fire
+    this.sub_pedidos = this.fire
       .getCollection('pedidos')
       .valueChanges()
       .subscribe((next) => {
